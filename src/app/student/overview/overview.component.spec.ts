24fb4380d642a569925ef40e616165b5b65a9773
Bruno Picolo <bruno.picolo@edataconsulting.de>
2023-11-20 08:32:08 +0000

    feat: should NOT show info banner if there are students
    I wasn't sure about how to inject the students list for this component. So, this commit is adding this functionality.
    
    TDD is an iterative process and this helped me with some technical issues.
    
    For the moment, I decided to pass the students as an input property. Why?
      - The following test has to verify that the info banner is not there when there are students
      - My test will set up some students in the component and check that the banner is not present
         - The students are not defined, so the test fails. Then, I define an empty list 'students'
         - The banner stills there, so the test fails. Then, I add an ng-container with an ngIf directive
             - I used the ng-container because it completely removes the rendering of the html inside it if the condition is false. A cleaner and faster rendering.
         - The test should work now, but it fails....
         - This is because I choose to use the OnPush change detection strategy... This OnPush is more performant but it makes harder to do Unit Tests.
             - I could identify faster from where the issue came from because I worked on tiny steps, so I knew the error should come from some configuration issue (the component config, the angular config or the jest config)
             - The change detection is only triggered if an input property is updated or if an observable (subscribed in the component) value comes in.
             - In my research I found this [angular github issue](https://github.com/angular/angular/issues/12313) where they mention the issue. There are 2 possible ways to solve this issue:
               - Changing the TestBed change detection to default. This is awful, because we are not testing the real thing...
               - Hacking a bit the fixture changeDetectionRef to detect the change. This may work but it messes with the Angular change detection, if Angular updates the way it works we are Fu**ed.
               - Creating a wrapper component that will pass the input to my component. This is a much cleaner solution, because it does not mess with Angular change detection. So, I used this one.
         - Because I have to trigger the change detection, I need to make the 'students' variable Input property so the change detection could be triggered by the wrapper component.
      - This is a choice of the moment, maybe during the process I decide that it will be better to trigger the changeDetection using an observable given by an studentService.
    
    Footer notes:
      - This was an interesting issue. Working iteratively I fixed easily the problem and learnt a bit more about angular change detection and jest testing.
      - Does it worth it to use OnPush change detection?
        - Well, at this stage it could be a bit overwhelming because with so few components we will definitively won't have performance issues.
        - In the other hand it will be more difficult to add the OnPush change detection in a more advanced setting.
        - This is my design decision, I chose to add it and fix the little issues that I will encounter than having a big issue in the future

--- src/app/student/overview/overview.component.spec.ts
+++ src/app/student/overview/overview.component.spec.ts
@@ -1,20 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OverviewComponent } from './overview.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wrapper',
+  standalone: true,
+  imports: [CommonModule, OverviewComponent],
+  template: '<students-dashboard [students]="content"></students-dashboard>'
+})
+class WrapperComponent {
+  content: string[] = [];
+}
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
 
+  let wrapperFixture: ComponentFixture<WrapperComponent>;
+  let wrapperComponent: WrapperComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OverviewComponent]
+      imports: [OverviewComponent, WrapperComponent]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    wrapperFixture = TestBed.createComponent(WrapperComponent);
+    wrapperComponent = wrapperFixture.componentInstance;
   });
 
   it('should create', () => {
@@ -31,5 +49,13 @@ describe('OverviewComponent', () => {
     const noStudents = fixture.nativeElement.querySelector('.tst-no-students');
     expect(noStudents).toBeTruthy();
     expect(noStudents?.textContent).toContain('There are no students!');
+  });
+
+  it('should NOT display an info when there are students', () => {
+    wrapperComponent.content = ['student1', 'student2'];
+    wrapperFixture.detectChanges();
+
+    const noStudents = wrapperFixture.nativeElement.querySelector('.tst-no-students');
+    expect(noStudents).toBeNull();
   });
 });
