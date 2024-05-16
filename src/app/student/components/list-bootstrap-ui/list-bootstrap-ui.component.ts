64be9b99c315a98be4b729ce4537b10046396bb5
Bruno Picolo <bruno.picolo@edataconsulting.de>
2024-05-16 15:38:54 +0100

    feat(student): add style to 'No students to show' banner

--- src/app/student/components/list-bootstrap-ui/list-bootstrap-ui.component.ts
+++ src/app/student/components/list-bootstrap-ui/list-bootstrap-ui.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Student } from '../../models/student.model';
 
 @Component({
-  selector: 'student-list-bootstrap-ui',
+  selector: 'student-list-bootstrap-ui[students]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './list-bootstrap-ui.component.html',
   styleUrl: './list-bootstrap-ui.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListBootstrapUiComponent {
-
+  @Input({ required: true })
+  students!: Student[];
 }
