0d9f748363357860ddf7acca2beb565bb86fbca8
Bruno Picolo <bruno.picolo@edataconsulting.de>
2024-05-18 13:21:34 +0100

    feat(student): integrate the student list to the /students
    This is mostly angular route configuration. As it's the beginning of the project, I wanted to keep this simpler.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'online-academy' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('online-academy');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, online-academy');
-  });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListComponent as StudentListComponent } from './student/components/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'students',
+    component: StudentListComponent
+  },
+];
